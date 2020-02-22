1d5a05441cda40137e7e11df6afec045b11d497c
Garen Yondem <garen@yondem.com>
2020-02-22 21:35:20 +0300

    Remove async prefix

--- app.ts
+++ app.ts
@@ -2,18 +2,18 @@ import express, { Request, Response } from "express";
 //@ts-ignore
 import randomWords from "random-words";
 
-async function eventsHandler(req: Request, res: Response) {
-  // Set & send required headers
-  res.writeHead(200, {
-    "Cache-Control": "no-cache",
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive"
-  });
+function eventsHandler(req: Request, res: Response) {
+    // Set & send required headers
+    res.writeHead(200, {
+        "Cache-Control": "no-cache",
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive"
+    });
 
-  setInterval(() => {
-    // emit
-    res.write(`data: ${randomWords()}\n\n`);
-  }, 200);
+    setInterval(() => {
+        // emit
+        res.write(`data: ${randomWords()}\n\n`);
+    }, 200);
 }
 
 const app = express();
