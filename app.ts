0bcd8b0e216f25937f0fdd3fd8f68339fae3e648
Garen Yondem <garen@yondem.com>
2020-02-22 21:16:09 +0300

    Init

---
+++ app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+//@ts-ignore
+import randomWords from "random-words";
+
+async function eventsHandler(req: Request, res: Response) {
+  // Set & send required headers
+  res.writeHead(200, {
+    "Cache-Control": "no-cache",
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive"
+  });
+
+  setInterval(() => {
+    // emit
+    res.write(`data: ${randomWords()}\n\n`);
+  }, 200);
+}
+
+const app = express();
+app.use("/", express.static("public"));
+app.get("/events", eventsHandler);
+app.listen(3020);
+
+console.log("localhost:3020");
